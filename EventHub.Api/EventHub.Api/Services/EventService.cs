b01c9dd9b95f2b8a92f164898d502bda397ff9a5
dimaassdf5 <dimaassdf5@gmail.com>
2026-07-15 08:31:35 +0300

    feat: add EventService

---
+++ EventHub.Api/EventHub.Api/Services/EventService.cs
@@ -0,0 +1,36 @@
+using EventHub.Api.Models;
+
+namespace EventHub.Api.Services;
+
+public class EventService : IEventService
+{
+    
+    // Коллекция для манипуляции над событиями
+    private static List<Event> Events { get; set; } = [];
+    
+    public List<Event> GetEvents() => Events;
+
+    public Event? GetEventById(Guid id) => Events.FirstOrDefault(x => x.Id == id);
+
+    public void CreateEvent(Event newEvent) => Events.Add(newEvent);
+
+    public Event? UpdateEvent(Event updatedEvent)
+    {
+        var index = Events.FindIndex(x => x.Id == updatedEvent.Id);
+        
+        if (index == -1)
+            return null;
+
+        Events[index] = updatedEvent;
+        return updatedEvent;
+    }
+
+    public bool DeleteEvent(Guid id)
+    {
+        var index = Events.FindIndex(x => x.Id == id);
+        if (index == -1) return false;
+
+        Events.RemoveAt(index);
+        return true;
+    }
+}

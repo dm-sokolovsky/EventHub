6e911b976e231e9066dea626cf9a73c324e23527
dimaassdf5 <dimaassdf5@gmail.com>
2026-07-15 10:31:28 +0300

    feat: add doc for Swagger

--- EventHub.Api/EventHub.Api/Controllers/EventsController.cs
+++ EventHub.Api/EventHub.Api/Controllers/EventsController.cs
@@ -5,10 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EventHub.Api.Controllers;
 
+/// <summary>
+/// Контроллер для манипуляции над событиями
+/// </summary>
+/// <param name="eventService"></param>
 [ApiController]
 [Route("api/[controller]")]
 public class EventsController(IEventService  eventService): ControllerBase
 {
+    /// <summary>
+    /// Метод возвращает все события из коллекции
+    /// </summary>
+    /// <response code="200">Возвращается JSON-структура ApiResult с деталями ответа</response>
+    /// <returns></returns>
+    [ProducesResponseType(typeof(ApiResult<List<EventDto>>), StatusCodes.Status200OK)]
+    [Produces("application/json")]
     [HttpGet]
     public ApiResult<List<EventDto>> GetAllEvents()
     {
@@ -25,6 +36,18 @@ public class EventsController(IEventService  eventService): ControllerBase
         };
     }
     
+    /// <summary>
+    /// Метод возвращает событие по id из коллекции
+    /// </summary>
+    /// <param name="id">Параметр id, для получения события</param>
+    /// <response code="200">Возвращается JSON-структура ApiResult с деталями ответа</response>
+    /// <response code="404">Возвращается JSON-структура ApiBaseResult с деталями ответа</response>
+    /// <response code="500">Возвращается JSON-структура ApiBaseResult с деталями ответа необработанного исключения</response>
+    /// <returns></returns>
+    [ProducesResponseType(typeof(ApiBaseResult), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiBaseResult), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ApiResult<EventDto>), StatusCodes.Status200OK)]
+    [Produces("application/json")]
     [HttpGet("{id}")]
     public ApiBaseResult GetEventById(Guid id)
     {
@@ -54,7 +77,7 @@ public class EventsController(IEventService  eventService): ControllerBase
         catch (ArgumentOutOfRangeException ex)
         {
             // В случае ошибки возвращаем неуспешный результат со статусом Not Found
-            return new ApiResult
+            return new ApiBaseResult
             {
                 Success = false,
                 StatusCode = HttpStatusCode.NotFound,
@@ -63,7 +86,7 @@ public class EventsController(IEventService  eventService): ControllerBase
         }
         catch (Exception ex)
         {
-            return new ApiResult
+            return new ApiBaseResult
             {
                 Success = false,
                 StatusCode = HttpStatusCode.InternalServerError,
@@ -72,12 +95,20 @@ public class EventsController(IEventService  eventService): ControllerBase
         }
     }
 
+    /// <summary>
+    /// Метод добавляет новое событие в коллекцию
+    /// </summary>
+    /// <param name="eventDto">Параметр eventDto, для добавления нового события в коллекцию</param>
+    /// <response code="201">Возвращается JSON-структура ApiResult с деталями ответа</response>
+    /// <returns></returns>
+    [ProducesResponseType(typeof(ApiResult), StatusCodes.Status200OK)]
+    [Produces("application/json")]
     [HttpPost]
-    public ApiResult CreateEvent(EventCreatedDto eventDto)
+    public ApiBaseResult CreateEvent(EventCreatedDto eventDto)
     {
         eventService.CreateEvent(MapEvent(eventDto));
         
-        return new ApiResult
+        return new ApiBaseResult
         {
             Success = true,
             StatusCode = HttpStatusCode.Created,
@@ -85,6 +116,17 @@ public class EventsController(IEventService  eventService): ControllerBase
         };
     }
 
+    /// <summary>
+    /// Метод обновляет событие из коллекции по id
+    /// </summary>
+    /// <param name="id">id событие</param>
+    /// <param name="eventDto">измененные данные события</param>
+    /// <response code="200">Возвращается JSON-структура ApiResult с деталями ответа</response>
+    /// <response code="404">Возвращается JSON-структура ApiBaseResult с деталями ответа</response>
+    /// <returns></returns>
+    [ProducesResponseType(typeof(ApiBaseResult), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResult<EventDto>), StatusCodes.Status200OK)]
+    [Produces("application/json")]
     [HttpPut("{id}")]
     public ApiBaseResult UpdateEvent(Guid id, EventCreatedDto eventDto)
     {
@@ -109,6 +151,16 @@ public class EventsController(IEventService  eventService): ControllerBase
         };
     }
 
+    /// <summary>
+    /// Метод удаляет событие из коллекции по id
+    /// </summary>
+    /// <param name="id">id события</param>
+    /// <response code="200">Возвращается JSON-структура ApiResult с деталями ответа</response>
+    /// <response code="404">Возвращается JSON-структура ApiBaseResult с деталями ответа</response>
+    /// <returns></returns>
+    [ProducesResponseType(typeof(ApiBaseResult), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiBaseResult), StatusCodes.Status200OK)]
+    [Produces("application/json")]
     [HttpDelete("{id}")]
     public ApiBaseResult DeleteEvent(Guid id)
     {
@@ -124,7 +176,7 @@ public class EventsController(IEventService  eventService): ControllerBase
             };
         }
         
-        return new ApiResult
+        return new ApiBaseResult()
         {
             Success = true,
             StatusCode = HttpStatusCode.OK,
--- EventHub.Api/EventHub.Api/Models/ApiResult.cs
+++ EventHub.Api/EventHub.Api/Models/ApiResult.cs
@@ -2,26 +2,45 @@ using System.Net;
 
 namespace EventHub.Api.Models;
 
+/// <summary>
+/// Кдасс с возвращаемыми данными
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class ApiResult<T> : ApiBaseResult
 {
-    // Возвращаемые данные метода
+    /// <summary>
+    /// Возвращаемые данные метода
+    /// </summary>
     public required T Data { get; set; }
 }
 
-// Класс ApiResult без возвращаемых данных
-// Наследуемся от базового класса с основными параметрами
+/// <summary>
+/// Класс ApiResult без возвращаемых данных
+/// Наследуемся от базового класса с основными параметрами
+/// </summary>
 public class ApiResult : ApiBaseResult { }
 
 
+/// <summary>
+/// Базовый класс с основными возвращаемыми параметрами
+/// </summary>
 public class ApiBaseResult
 {
-    // Флаг, указывающий на успешность выполненного запроса
+    /// <summary>
+    /// Флаг, указывающий на успешность выполненного запроса
+    /// </summary>
     public required bool Success { get; set; }
-    // Возвращаемый HTTP-код
+    /// <summary>
+    /// Возвращаемый HTTP-код
+    /// </summary>
     public required HttpStatusCode StatusCode { get; set; }
-    // Дата и время ответа
+    /// <summary>
+    /// Дата и время ответа
+    /// </summary>
     public DateTime DateTime { get; set; } = DateTime.UtcNow;
-    // Кастомное сообщение с дополнительной информацией
-    // Здесь может быть информация об ошибке в случае неуспеха
+    /// <summary>
+    /// Кастомное сообщение с дополнительной информацией
+    /// Здесь может быть информация об ошибке в случае неуспеха
+    /// </summary>
     public required string Message { get; set; }
 } 

cd902bbe7a4ca2f564f6c696045010c70bbad918
dimaassdf5 <dimaassdf5@gmail.com>
2026-09-22 10:52:23 +0300

    feat: Реализовал EventRepository

---
+++ EventHub.Api/EventHub.Api/DataAccess/Repositories/Abstractions/IEventRepository.cs
@@ -0,0 +1,13 @@
+using EventHub.Api.Contracts;
+using EventHub.Api.Models;
+
+namespace EventHub.Api.DataAccess.Repositories.Abstractions;
+
+public interface IEventRepository
+{
+    Task AddAsync(Event data, CancellationToken ct = default);
+    Task<Event?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<(IQueryable<Event>, int totalCount)> GetAllEventsAsync(EventFilter filter,CancellationToken ct = default);
+    Task<Event?> UpdateByIdAsync(Guid id, EventUpsert data, CancellationToken ct = default);
+    Task<bool> DeleteByIdAsync(Guid id, CancellationToken ct = default);
+}
--- EventHub.Api/EventHub.Api/Services/EventService.cs
+++ EventHub.Api/EventHub.Api/Services/EventService.cs
@@ -1,33 +1,29 @@
 using EventHub.Api.Common.Exceptions;
 using EventHub.Api.Contracts;
 using EventHub.Api.DataAccess;
+using EventHub.Api.DataAccess.Repositories;
 using EventHub.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventHub.Api.Services;
 
-public sealed class EventService : IEventService
+public sealed class EventService(EventRepository eventRepository) : IEventService
 {
-
-    private readonly AppDbContext _context;
-
-    public EventService(AppDbContext context)
-    {
-        _context = context;
-    }
+    
+    private readonly EventRepository _eventRepository = eventRepository;
     
     public async Task<EventInfo> CreateEventAsync(CreateEvent request, CancellationToken cancellationToken = default)
     {
         var @event = Event.Create(request.Title, request.StartAt, request.EndAt, request.TotalSeats, request.Description);
-        await _context.Events.AddAsync(@event, cancellationToken);
-        await _context.SaveChangesAsync(cancellationToken);
+        
+        await _eventRepository.AddAsync(@event, cancellationToken);
+        
         return ToInfo(@event);
     }
     
     public async Task<EventInfo> GetEventByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        var @event = await _context.Events.FirstOrDefaultAsync(e => e.Id == id, cancellationToken)
-                     ?? throw new NotFoundException("Event not found");
+        var @event = await _eventRepository.GetByIdAsync(id, cancellationToken) ?? throw new NotFoundException("Event not found");
 
         return ToInfo(@event);
     }
@@ -38,18 +34,7 @@ public sealed class EventService : IEventService
         int pageSize = 10, 
         CancellationToken cancellationToken = default)
     {
-        var query = _context.Events.AsQueryable();
-
-        if (eventFilter.From.HasValue)
-            query = query.Where(e => e.StartAt >= eventFilter.From.Value);
-
-        if (eventFilter.To.HasValue)
-            query = query.Where(e => e.StartAt <= eventFilter.To.Value);
-
-        if (!string.IsNullOrWhiteSpace(eventFilter.Title))
-            query = query.Where(e => e.Title.ToLower().Contains(eventFilter.Title.ToLower()));
-
-        var totalCount = await query.CountAsync(cancellationToken);
+        var (query, totalCount) = await _eventRepository.GetAllEventsAsync(eventFilter, cancellationToken);
 
         var items = await query
             .Skip((page - 1) * pageSize)
@@ -67,24 +52,15 @@ public sealed class EventService : IEventService
     
     public async Task<EventInfo> UpdateEventAsync(Guid id, EventUpsert request, CancellationToken cancellationToken = default)
     {
-        var @event = await _context.Events.FirstOrDefaultAsync(e => e.Id == id, cancellationToken)
+        var @event = await _eventRepository.UpdateByIdAsync(id, request, cancellationToken)
                      ?? throw new NotFoundException("Event not found");
-
-        @event.Update(request.Title, request.StartAt, request.EndAt, request.Description);
-        await _context.SaveChangesAsync(cancellationToken);
 
         return ToInfo(@event);
     }
 
     public async Task<bool> DeleteEventAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        var @event = await _context.Events.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
-        if (@event == null)
-            return false;
-
-        _context.Events.Remove(@event);
-        await _context.SaveChangesAsync(cancellationToken);
-        return true;
+        return await _eventRepository.DeleteByIdAsync(id, cancellationToken);;
     }
 
     
